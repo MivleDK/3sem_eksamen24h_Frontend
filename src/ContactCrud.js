9c186dad5304f1c650d3f3fcfc483b8b0b08738d
Mick <grevmivlos@gmail.com>
2021-01-19 00:35:07 +0100

    Add partially working - moving on to edit

--- src/ContactCrud.js
+++ src/ContactCrud.js
@@ -1,17 +1,17 @@
-import React, {useState, useEffect} from "react";
-import {AllUsers, DeleteUser, UpdateUser, GetUser, AddUser, URLAllContacts} from "./settings";
-import{
-  Container, 
-  Row, 
+import React, { useState, useEffect } from "react";
+import { AllUsers, URLDeleteContact, URLUpdateContact, URLGetContact, URLAddContact, URLAllContacts } from "./settings";
+import {
+  Container,
+  Row,
   Col,
   Button,
   InputGroup,
   FormControl,
-  Table, 
-  Form
+  Table,
+  Form,
 } from "react-bootstrap";
 
-function ContactCrud(){
+function ContactCrud() {
   const initialValues = {
     name: "",
     email: "",
@@ -26,68 +26,211 @@ function ContactCrud(){
 
   useEffect(() => {
     fetchContacts();
-  }, []);  
+  }, []);
 
-  const fetchContacts = () =>{
+  const fetchContacts = () => {
     fetch(URLAllContacts)
-    .then((res) => res.json())
-    .then((data) => {
-      setAllContacts(data)
-      console.log(data);
-    });
+      .then((res) => res.json())
+      .then((data) => {
+        setAllContacts(data);
+        console.log(data);
+      });
   };
 
-  const contactsFormTable = () =>{
-    return(
-    <div>
-      <Container>
-        <h2>Contacts CRUD</h2>
-        <Row className="mt-4">
-          <Col>
-            <Table bordered>
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Job title</th>
-                  <th>Company</th>                  
-                  <th>Phone</th>
-                  <th>Email</th>
-                </tr>
-              </thead>
-              <tbody>
-                {allContacts.all && allContacts.all.map((elem) =>{
-                  return (
-                    <tr key={elem.email}>
-                      <td>{elem.name}</td>
-                      <td>{elem.jobTitle}</td>
-                      <td>{elem.company}</td>
-                      <td>{elem.phone}</td>
-                      <td>{elem.email}</td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </Table>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-    )};
+  const fetchContact = (email) => {
+    fetch(URLGetContact + email)
+      .then((res) => res.json())
+      .then((data) => {
+        setContact(data);
+      })
+      .catch((err) => {
+        if (err.status) {
+          err.fullError.then((e) => console.log(e.detail));
+        } else {
+          console.log("Network error");
+        }
+      });
+  };
 
-  const contactsForm  = () =>{
-    return(
-      <div></div>
-    )
+  const deletePerson = (email) => {
+    const options = makeOptions("DELETE");
+
+    fetch(URLDeleteContact);
+  };
+
+
+  /*
+  Function for POST, PUT and DELETE
+  */
+  function makeOptions(method, body) {
+    const opts = {
+      method: method,
+      headers: {
+        "Content-type": "application/json",
+        Accept: "application/json",
+      },
+    };
+    if (body) {
+      opts.body = JSON.stringify(body);
+    }
+    return opts;
   }
 
+  const handleChange = (event) => {
+    const target = event.target;
+    const id = target.id;
+    const value = target.value;
+    setContact({ ...contact, [id]: value });
+  };  
 
-  return(
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    updateForm(contact);
+  };
+
+  const addContact = () => {
+    const options = makeOptions("POST", contact);
+    console.log(contact.email);
+    fetch(URLAddContact, options)
+      .then((res) => res.json())
+      .then((res) => fetchContact())
+      .catch((err) => {
+        if (err.status) {
+          err.fullError.then((e) => console.log(e.detail));
+        } else {
+          console.log("Network error");
+        }
+      });
+  };
+
+  const updateForm = (contact) => {
+    const options = makeOptions("PUT", contact);
+
+    fetch(URLUpdateContact, options)
+      .then((res) => fetchContact())
+      .catch((err) => {
+        if (err.status) {
+          err.fullError.then((e) => console.log(e.detail));
+        } else {
+          console.log("Network error" + err);
+        }
+      });
+  };
+
+
+const userForm = () =>{
+  return (
     <div>
-      {contactsFormTable()}
-      {contactsForm()}
+      <Form inline onSubmit={handleSubmit}>
+        <Form.Group className="mr-2" controlId="name">
+          <Form.Control
+            size="sm"
+            type="text"
+            placeholder="Name"
+            value={contact.name}
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Form.Group className="mr-2" controlId="jobTitle">
+          <Form.Control
+            size="sm"
+            type="text"
+            placeholder="Job title"
+            value={contact.jobTitle}
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Form.Group className="mr-2" controlId="company">
+          <Form.Control
+            size="sm"
+            type="text"
+            placeholder="Company name"
+            value={contact.company}
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Form.Group className="mr-2" controlId="phone">
+          <Form.Control
+            size="sm"
+            type="text"
+            placeholder="Phone number"
+            value={contact.phone}
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Form.Group className="mr-2" controlId="email">
+          <Form.Control
+            size="sm"
+            type="email"
+            placeholder="Email address"
+            value={contact.email}
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Button size="sm" variant="success" onClick={() => addContact()}>
+          Add Contact
+        </Button>
+      </Form>
     </div>
-  )
+  );};
 
+  const contactsFormTable = () => {
+    return (
+      <div>
+        <Container>
+          <Row className="mt-4">
+            <Col>
+              <Table bordered>
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Job title</th>
+                    <th>Company</th>
+                    <th>Phone</th>
+                    <th>Email</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {allContacts.all &&
+                    allContacts.all.map((elem) => {
+                      return (
+                        <tr key={elem.email}>
+                          <td>{elem.name}</td>
+                          <td>{elem.jobTitle}</td>
+                          <td>{elem.company}</td>
+                          <td>{elem.phone}</td>
+                          <td>{elem.email}</td>
+                          <td>
+                            <Button variant="warning" onClick={() => fetchContact(elem.email)}>
+                              Edit
+                            </Button>
+                          </td>
+                          <td>
+                            <Button variant="danger" onClick={() => deletePerson(elem.email)}>
+                              Delete
+                            </Button>
+                          </td>
+                        </tr>
+                      );
+                    })}
+                </tbody>
+              </Table>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <Container>
+        <h2 className="mb-4">Contacts CRUD</h2>
+        {userForm()}
+        </Container>
+      {contactsFormTable()}
+    </div>
+  );
 }
 
 export default ContactCrud;
